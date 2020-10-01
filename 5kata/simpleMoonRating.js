c27e0e1e6efcda6fe532999de38fd5765cbb56f2
James Olivas <jamesbolivas@gmail.com>
2020-09-30 17:00:14 -0700

    Update solution

--- 5kata/simpleMoonRating.js
+++ 5kata/simpleMoonRating.js
@@ -1,3 +1,1 @@
-//Note: current solution is incomplete. The function requires <= 64 characters, this solution has 72
-
-moonRating=r=>[0,2,4,6,8].map(i=>r-i>=1.5?'o':r-i>=0.5?'c':'x').join('')
+moonRating=r=>'xxxxx'.replace(/x/g,d=>r--<.5?'x':r--<.5?'c':'o')
